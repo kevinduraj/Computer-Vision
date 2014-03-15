d6371b4cb97c9065d1af7e81db114d06bc3b0fca
Kevin Duraj <kevinduraj@gmail.com>
2014-03-15 15:04:10 -0700

    compute cutoff low and cutoff high

--- Project02/src/project02/Histogram.java
+++ Project02/src/project02/Histogram.java
@@ -3,16 +3,28 @@ package project02;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import javax.imageio.ImageIO;
 
 public class Histogram {
 
-    Map<Integer, Integer> map = new TreeMap();
+    private Map<Integer, Integer> mapSource = new TreeMap();
+    private Map<Integer, Integer> mapFinal = new TreeMap();
     
+    private Integer totalPixels;
+    private Integer cutoff;
+    private Integer cutoffHigh;
+
+
     /*--------------------------------------------------------------------------------------------*/
+    public Histogram() {
+        totalPixels = 0;
+        cutoff = 0;
+        cutoffHigh = 0;
+    }
+    /*--------------------------------------------------------------------------------------------*/
+
     public void readHistogram(String filename) {
 
         try {
@@ -24,8 +36,10 @@ public class Histogram {
 
             for (int i = 0; i < grn.length; ++i) {
                 for (int j = 0; j < grn[i].length; ++j) {
+                    
+                    totalPixels ++;
                     grn[i][j] = bi.getRGB(j, i) >> 8 & 0xFF;
-                    incrementValue(grn[i][j], 1);
+                    incrementSource(grn[i][j], 1);                   
                 }
             }
 
@@ -34,22 +48,79 @@ public class Histogram {
         }
     }
     /*--------------------------------------------------------------------------------------------*/
-    public void incrementValue(Integer key, Integer value) {
 
-        int count = map.containsKey(key) ? map.get(key) : 0;
+    public void incrementSource(Integer key, Integer value) {
+
+        int count = mapSource.containsKey(key) ? mapSource.get(key) : 0;
         count += value;
-        map.put(key, count);
-
+        mapSource.put(key, count);
     }
+    /*--------------------------------------------------------------------------------------------*/
+
+    public void incrementFinal(Integer key, Integer value) {
+
+        int count = mapFinal.containsKey(key) ? mapFinal.get(key) : 0;
+        count += value;
+        mapFinal.put(key, count);
+
+    }   
 
     /*--------------------------------------------------------------------------------------------*/
     public void displayHistogram() {
 
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : mapSource.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println(key + " => " + value);
+        }
+    }
+    /*--------------------------------------------------------------------------------------------*/
+    public void displayCutoff() {
+
+        for (Map.Entry<Integer, Integer> entry : mapFinal.entrySet()) {
             Integer key = entry.getKey();
             Integer value = entry.getValue();
             System.out.println(key + " => " + value);
         }
     }    
     /*--------------------------------------------------------------------------------------------*/
+    public void cutoffHistogram() {
+
+        int sum = 0;
+        
+        for (Map.Entry<Integer, Integer> entry : mapSource.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+            
+            sum += value;
+            if(sum < cutoff) mapFinal.put(key, 0);
+            else if(sum > cutoffHigh) mapFinal.put(key, 0);
+            else mapFinal.put(key, value);
+        }
+        
+        
+    }    
+    /*--------------------------------------------------------------------------------------------*/
+    public void setCutoff(int percent) {
+
+        cutoff = (int) (totalPixels * 0.1);   
+        cutoffHigh = totalPixels - cutoff;
+    }
+    /*--------------------------------------------------------------------------------------------*/
+    public Integer getCutoff() {
+        if (cutoff == 0)
+            cutoff = (int) (totalPixels * 0.1);   
+        return cutoff;
+    }    
+    /*--------------------------------------------------------------------------------------------*/
+    public void displayAll() {
+
+        System.out.println("\n-------------------------------------\n");
+        System.out.println("Total Pixels: " + totalPixels);
+        System.out.println("Cutoff      : " + cutoff);
+        System.out.println("CutoffHigh  : " + cutoffHigh);
+        
+          
+    }    
+    /*--------------------------------------------------------------------------------------------*/    
 }
--- Project02/src/project02/Main.java
+++ Project02/src/project02/Main.java
@@ -1,8 +1,5 @@
 package project02;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -12,6 +9,13 @@ public class Main {
         /*---------------- Loading Data -------------------------*/
         Histogram histogram = new Histogram();
         histogram.readHistogram("src/image2/Lenna.png");
-        histogram.displayHistogram();
+        histogram.setCutoff(10);
+        //histogram.displayHistogram(); 
+        /*-------------------------------------------------------*/
+        histogram.displayAll();
+        histogram.cutoffHistogram();
+        histogram.displayCutoff();
+        /*-------------------------------------------------------*/
+             
     }
 }
